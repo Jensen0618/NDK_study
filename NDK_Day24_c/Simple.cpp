74b58e42e3407f539a7bebcde47a70ca6d964b43
Jensen0618 <jensenlinjensen@gmail.com>
2025-01-05 20:36:29 +0800

    message

--- NDK_Day24_c/Simple.cpp
+++ NDK_Day24_c/Simple.cpp
@@ -97,16 +97,24 @@ void add(float a, float b) {
 }
 
 
+//模板函数
+//相当于Java中的泛型
+template<typename T>
+T addd(T num1, T num2) {
+	return num1 + num2;
+}
+
+
 
 void main() {
 
 
 	C c("Jensen", 34);
 
-	F f = new F("f");
+	F* f = new F("f");
 	//编译器不能确定调用的是A还是C的_name()方法
 	//加了virtual怎么还是不行啊？？？
-	f._name();
+	//f->_name();
 
 
 	//多态需要使用子类指向父类
@@ -118,6 +126,12 @@ void main() {
 	hello(e);
 
 	//静态多态就是函数的重载，像同一个方法有不同的参数就是了
+
+	//为了解决静态多态需要写多个函数的问题，模板函数出现了，相当于Java中的泛型
+	cout << addd(1, 2) << endl;
+	cout << addd(1.0f, 2.0f) << endl;
+	cout << addd(1, 2) << endl;
+
 
 	getchar();
 
