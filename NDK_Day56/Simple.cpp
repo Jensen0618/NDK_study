426ca5b6fd4fc59ce9baf8040a0b1259f280e022
JensenLin <jensenlin0215@gmail.com>
2025-08-01 00:07:03 +0800

    彩色转黑白

--- NDK_Day56/Simple.cpp
+++ NDK_Day56/Simple.cpp
@@ -8,16 +8,43 @@ void main(){
 	//读取一张图片，返回Mat对象
 	Mat src = imread("D:/photo.jpg");
 
-	Mat dst;
+	//Mat dst;
 
-	cvtColor(src, dst, COLOR_BGR2GRAY);
+	//cvtColor(src, dst, COLOR_BGR2GRAY);
 
 
 	//将Mat对象写入文件
-	imwrite("D:/copy.jpg", dst);
+	//imwrite("D:/copy.jpg", dst);
+
+	for (size_t i = 0; i < src.rows; i++)
+	{
+		uchar* point = src.ptr<uchar>(i); //获取第i行的首指针
+
+		for (size_t j = 0; j < src.cols; j++)
+		{
+			//获取每个像素点的BGR值
+			//Vec3b bgr = src.at<Vec3b>(i, j);
+			//输出每个像素点的BGR值
+			//printf("第%d行，第%d列的像素点的BGR值为：%d,%d,%d\n", i, j, bgr[0], bgr[1], bgr[2]);
+			//取出每一个像素点的BGR值
+			uchar b = point[j * 3 + 0]; //蓝色通道
+			uchar g = point[j * 3 + 1]; //绿色通道
+			uchar r = point[j * 3 + 2]; //红色通道
+
+			uchar gray = 0.11 * b + 0.59 * g + 0.3 * r; //灰度值
+
+			point[j * 3 + 0] = gray; //蓝色通道
+			point[j * 3 + 1] = gray; //绿色通道
+			point[j * 3 + 2] = gray; //红色通道
+		}
+
+	}
 
 	//显示图片
-	imshow("test", dst);
+	imshow("test", src);
+
+	//宽、高、通道数
+	printf("图片的宽度：%d，图片的高度：%d，图片的通道数：%d\n", src.cols,src.rows,src.channels());
 
 	//openCV的等待
 	waitKey(0);
